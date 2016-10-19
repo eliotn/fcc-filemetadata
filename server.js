e285ee21f15b0c31be851319c9be74837f98f440
Eliot Glairon <ejg_dnd@yahoo.com>
2016-10-19 16:00:05 +0000

    Server with multer

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express')
+var multer = require('multer')
 var app = express();
+var busboy = require('busboy')
+//1. save to memory storage
+//2. set limits for the file
+var upload = multer({storage: multer.memoryStorage(), ""})
+app.use(express.static(__dirname + '/public'));
 
+app.post('/upload', upload.single('fileToUpload'), function(req, res, next) {
+    
+    res.json({"size":req.file.size});
+})
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
