63f8475db4a0d5322e58d88214581246928de35b
Eliot Glairon <ejg_dnd@yahoo.com>
2016-10-21 21:27:38 +0000

    Final changes!

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname + '/public'));
 
 //don't save to storage, just get the number of bytes and add them together
 app.post('/upload', function(req, res) {
-    var busboy = new Busboy({ headers: req.headers });
+    var busboy = new Busboy({ headers: req.headers, limits: {files: 1, fileSize: 10485760, parts: 40960, fields: 40960}});
     busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
       console.log('File [' + fieldname + ']: filename: ' + filename);
       var bytes = 0;
@@ -17,6 +17,9 @@ app.post('/upload', function(req, res) {
         console.log('File [' + fieldname + '] Finished');
         res.json({"size":bytes});
       });
+      file.on('limit', function() {
+        res.json({"size":bytes, "error":"exceeded limits for file uploading (most likely number of files or filesize)"})
+      })
     });
     req.pipe(busboy);
 })
